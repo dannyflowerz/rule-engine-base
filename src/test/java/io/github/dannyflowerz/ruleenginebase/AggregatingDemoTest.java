492284af1688a714d6b1cb718093a4b2fdfe5dd6
dannyflowerz <daniel.gabor.flores@gmail.com>
2018-08-21 11:58:50 +0200

    migrated to JUnit5

--- src/test/java/io/github/dannyflowerz/ruleenginebase/AggregatingDemoTest.java
+++ src/test/java/io/github/dannyflowerz/ruleenginebase/AggregatingDemoTest.java
@@ -3,21 +3,25 @@ package io.github.dannyflowerz.ruleenginebase;
 import io.github.dannyflowerz.ruleenginebase.example.AggregatingRuleEngineExample;
 import io.github.dannyflowerz.ruleenginebase.example.Decision;
 import io.github.dannyflowerz.ruleenginebase.example.Input;
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class AggregatingDemoTest {
+
+class AggregatingDemoTest {
 
     private AggregatingRuleEngineExample aggregating = new AggregatingRuleEngineExample(true);
 
     @Test
-    public void should_decide_accepted_for_FOO() {
+    @DisplayName("Should decide accepted for FOO")
+    void evaluateFoo() {
         assertTrue(aggregating.evaluate(new Input(Input.Discriminator.FOO)).get().isAccepted());
     }
 
     @Test
-    public void should_decide_not_accepted_for_BAR_with_concatenated_reason() {
+    @DisplayName("Should decide not accepted for BAR with concatenated reason")
+    void evaluateBar() {
         Decision decision = aggregating.evaluate(new Input(Input.Discriminator.BAR)).get();
 
         assertFalse(decision.isAccepted());
--- src/test/java/io/github/dannyflowerz/ruleenginebase/ExclusiveDemoTest.java
+++ src/test/java/io/github/dannyflowerz/ruleenginebase/ExclusiveDemoTest.java
@@ -2,29 +2,33 @@ package io.github.dannyflowerz.ruleenginebase;
 
 import io.github.dannyflowerz.ruleenginebase.example.ExclusiveRuleEngineExample;
 import io.github.dannyflowerz.ruleenginebase.example.Input;
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class ExclusiveDemoTest {
+class ExclusiveDemoTest {
 
     private ExclusiveRuleEngineExample exclusive = new ExclusiveRuleEngineExample(true, false);
     private ExclusiveRuleEngineExample exclusiveMessedUp = new ExclusiveRuleEngineExample(true, true);
 
     @Test
-    public void should_decide_accepted_for_FOO() {
+    @DisplayName("Should decide accepted for FOO")
+    void evaluateFoo() {
         assertTrue(exclusive.evaluate(new Input(Input.Discriminator.FOO)).get().isAccepted());
     }
 
     @Test
-    public void should_decide_not_accepted_for_BAR() {
+    @DisplayName("Should decide not accepted fo BAR")
+    void evaluateBar() {
         assertFalse(exclusive.evaluate(new Input(Input.Discriminator.BAR)).get().isAccepted());
     }
 
-    @Test(expected = RuleException.class)
-    public void should_throw_RuleException_if_more_than_one_rule_applies() {
-        exclusiveMessedUp.evaluate(new Input(Input.Discriminator.BAR));
+    @Test
+    @DisplayName("Should throw RuleException if more than one rule applies")
+    void wrongSetUp() {
+        RuleException exception = assertThrows(RuleException.class, () -> exclusiveMessedUp.evaluate(new Input(Input.Discriminator.BAR)));
+        assertTrue(exception.getMessage().startsWith("Multiple rules applied for input"));
     }
 
 }
